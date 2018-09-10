7a7e35a943489a0d12115a2e4b18a87d7ce7ad1b
chenjingxiu <chenjingxiu@gomeplus.com>
2018-09-10 17:13:03 +0800

    add config paser

--- config.go
+++ config.go
@@ -1,66 +1,144 @@
-package midware
+/**
+ * [Gome Wap!] (C)2013-2023 Gome Inc.
+ * This is NOT a freeware, use is subject to license terms
+ *
+ * path   go-push/config
+ * date   2018/9/7 10:14
+ * author chenjingxiu
+ */
+package config
 
-// ------------------------------------------------------------------------------------------------
-//Database default config
-const (
-	DefaultDBDriver  = "mysql"
-	DefaultDBPrefix  = ""
-	DefaultDBShowSQL = true
-	DefaultDBMaxOpen = 10
-	DefaultDBMaxIdle = 10
-	DefaultDBDSN     = "root:123456@(127.0.0.1:3306)/test"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"sync"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"path/filepath"
 )
 
-//--------------------------------------------------------------------------------------------------
-//RabbitMQ
-//ExchangeMapping　the mapping of device and exchange
-const (
-	EXCHANGE_DEFAULT = iota
-	EXCHANGE_ANDROID
-	EXCHANGE_XIAOMI
-	EXCHANGE_HUAWEI
-	EXCHANGE_IOS
-)
-
-var ExchangeMapping = map[int]string{
-	EXCHANGE_ANDROID: AndroidExchangeName,
-	EXCHANGE_XIAOMI:  MIExchangeName,
-	EXCHANGE_HUAWEI:  HWExchangeName,
-	EXCHANGE_IOS:     IOSExchangeName,
-	EXCHANGE_DEFAULT: DefaultExchangeName,
+type GlobalConfig struct {
+	Database DB     `json:"database"`
+	Redis    RD     `json:"redis"`
+	RabbitMQ MQ     `json:"rabbitMQ"`
+	Mongodb  Mog    `json:"mongodb"`
+	Push     Secret `json:"push"`
+	Api      Inter  `json:"api"`
 }
-//Supported Exchange List
-const (
-	DefaultQueueName    = "default"
-	DefaultExchangeName = "default"
-	DefaultRoutingKey   = "default"
-	IOSQueueName        = "ios"
-	IOSExchangeName     = "ios"
-	IOSRoutingKey       = "ios"
-	AndroidQueueName    = "android"
-	AndroidExchangeName = "android"
-	AndroidRoutingKey   = "android"
-	MIQueueName         = "xiaomi"
-	MIExchangeName      = "xiaomi"
-	MIRoutingKey        = "xiaomi"
-	HWQueueName         = "huawei"
-	HWExchangeName      = "huawei"
-	HWRoutingKey        = "huawei"
+
+type DB struct {
+	Default DBItem `json:"default"`
+}
+
+type DBItem struct {
+	Driver  string `json:"driver"`
+	Dsn     string `json:"dsn"`
+	Prefix  string `json:"prefix"`
+	MaxOpen int    `json:"maxOpen"`
+	MaxIdle int    `json:"maxIdle"`
+	ShowSQl bool   `json:"showSQL"`
+}
+
+type RD struct {
+	Default RDItem `json:"default"`
+	Master  RDItem `json:"master"`
+	Slave   RDItem `json:"slave"`
+}
+
+type RDItem struct {
+	Url string `json:"url"`
+}
+
+type MQ struct {
+	Default MQItem `json:"default"`
+}
+
+type MQItem struct {
+	Name        string `json:"name"`
+	Url         string `json:"url"`
+	RoutingKey  string `json:"routingKey"`
+	Dsn         string `json:"dsn"`
+	VirtualHost string `json:"virtualHost"`
+}
+
+type Secret struct {
+	HuaWei SecretItem `json:"HW"`
+	Mi     SecretItem `json:"MI"`
+}
+
+type SecretItem struct {
+	Name       string `json:"name"`
+	AppId      string `json:"appId"`
+	AppSecret  string `json:"appSecret"`
+	Icon       string `json:"icon"`
+	IntentHead string `json:"intentHead"`
+	IntentTail string `json:"intentTail"`
+	Package    string `json:"package"`
+}
+
+type Mog struct {
+	Default MogItem `json:"default"`
+}
+
+type MogItem struct {
+	Url      string `json:"url"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	DBName   string `json:"dbName"`
+	Dsn      string `json:"dsn"`
+}
+
+type Inter struct {
+	Url map[string]string `json:"url"`
+}
+
+var (
+	GConfig    *GlobalConfig
+	configLock = new(sync.Mutex)
 )
-const DefaultExchangeType = "direct"
 
-//Queue Server
-const QueueServer = "amqp://guest:guest@127.0.0.1:5672/"
+func init() {
+	fileName, err := filepath.Abs("config.json")
+	if err != nil {
+		panic(err)
+	}
+	err = loadConfig(fileName)
+	if err != nil {
+		panic(err)
+	}
+	s := make(chan os.Signal, 1)
+	signal.Notify(s, syscall.SIGINT)
+	go func() {
+		for {
+			<-s
+			loadConfig(fileName)
+			fmt.Println("reload the config file")
+		}
+	}()
+}
 
-//-----------------------------------------------------------------------------------------------------
-//Mongodb
-const (
-	MogServers = "127.0.0.1:27017"
-	MogUser    = ""
-	MogPwd     = ""
-	MogDBName  = "test"
-)
+func GetConfig() *GlobalConfig {
+	configLock.Lock()
+	defer configLock.Unlock()
+	return GConfig
+}
 
-//------------------------------------------------------------------------------------------------------
-//redis
-const RedisServers = "127.0.0.1:6709;127.0.0.1:6707;127.0.0.1:6708"
+func loadConfig(file string) error {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		fmt.Println("read config file error: ", err)
+		return err
+	}
+	config := new(GlobalConfig)
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		fmt.Println("Unmarshal the config file to struct error: ", err)
+		return err
+	}
+	configLock.Lock()
+	GConfig = config
+	configLock.Unlock()
+	return nil
+}
---
+++ config_test.go
@@ -0,0 +1,21 @@
+/**
+ * [Gome Wap!] (C)2013-2023 Gome Inc.
+ * This is NOT a freeware, use is subject to license terms
+ *
+ * path   go-push/main
+ * date   2018/9/10 17:03 
+ * author chenjingxiu
+ */
+package config
+
+import (
+	"testing"
+	"fmt"
+)
+
+func BenchmarkGetConfig(t *testing.B) {
+	for i := 0; i < t.N; i++ {
+		c := GetConfig()
+		fmt.Println(c.Database.Default.Driver)
+	}
+}
